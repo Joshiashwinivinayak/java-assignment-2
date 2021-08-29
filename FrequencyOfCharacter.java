1da632061c2255852ddea6e2d880c8f28415c814
Joshiashwinivinayak <89513242+Joshiashwinivinayak@users.noreply.github.com>
2021-08-29 14:18:37 +0530

    Add files via upload

---
+++ FrequencyOfCharacter.java
@@ -0,0 +1,15 @@
+//Java Program to Find the Frequency of Characters in a String
+
+public class FrequencyOfCharacter {
+	public static void main(String[] args) {
+		String str = "This website is awesome.";
+		char ch = 'e';
+		int frequency = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (ch == str.charAt(i)) {
+				++frequency;
+			}
+		}
+		System.out.println("Frequency of " + ch + " = " + frequency);
+	}
+}
