cf08e3ab09dd1d09bdef2ae2223f75d3772cbf8f
Joshiashwinivinayak <89513242+Joshiashwinivinayak@users.noreply.github.com>
2024-01-10 16:18:09 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,12 @@
+class Demo {
+public static void main(String [] args){
+int x = 10;
+int y = 20;
+if(x > y){
+System.out.println(x+" is grater");
+}
+else {
+System.out.println(y+" is greater");
+}
+}
+}
